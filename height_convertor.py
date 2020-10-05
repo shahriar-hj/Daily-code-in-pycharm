e365823e00f4d9e807bf5232b33539291296759b
shahriar_hj <shahriarhajiabbasi@gmail.com>
2020-10-06 00:41:11 +0330

    height meter to feet converte

---
+++ height_convertor.py
@@ -0,0 +1,12 @@
+# this code will produce an app that convert the height...
+from tkinter import Tk, Button, Label, DoubleVar, Entry
+
+# build interface
+window = Tk()
+window.title("transform height in meter to feet")
+window.configure(background="light blue")
+window.geometry("320x220")
+window.resizable(width=False, height=False)
+
+
+window.mainloop()
